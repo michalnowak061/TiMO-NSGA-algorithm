516787ec0719c2a4ee61c7e1ac423d882a87457b
Michał Nowak <michal.nowak061@gmail.com>
2020-03-04 14:34:10 +0100

    Utworzenie pustego projektu i zalaczenie biblioteki exprtk.hpp do obslugi wyrazen matematycznych

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TiMO_Projekt
+//
+//  Created by Michal_Nowak & Kacper_Stenka on 02/03/2020.
+//  Copyright © 2020 Michal_Nowak & Kacper_Stenka. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main( void ) {
+    
+    cout << "Hello, World!\n";
+    
+    return 0;
+}
