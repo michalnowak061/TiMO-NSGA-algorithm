d35fd4fc9c5e3f83079af7b334c2b1c76ce38ad1
Michał Nowak <michal.nowak061@gmail.com>
2020-03-05 09:30:54 +0100

    Utworzenie klasy obslugujacej wyrazenia matematyczne

---
+++ Math_Parser.cpp
@@ -0,0 +1,72 @@
+//
+//  Math_Parser.cpp
+//  TiMO_Projekt
+//
+//  Created by Michal on 04/03/2020.
+//  Copyright © 2020 Michal_Nowak & Kacper_Stenka. All rights reserved.
+//
+
+#include "Math_Parser.hpp"
+
+// --------------------------------------------------------------------
+
+Math_Parser::Math_Parser() {
+
+}
+
+// --------------------------------------------------------------------
+
+Math_Parser::~Math_Parser() {
+    
+}
+
+// --------------------------------------------------------------------
+
+void Math_Parser::Math_Parser_Add_Expression(string expression) {
+    
+    Expression_String = expression;
+}
+
+// --------------------------------------------------------------------
+
+void Math_Parser::Math_Parser_Clear_Expression() {
+    
+    Expression_String.clear();
+}
+
+// --------------------------------------------------------------------
+
+VARIABLE_TYPE Math_Parser::Math_Parser_Get_Expression_Value() {
+    
+    return Expression_Value;
+}
+
+// --------------------------------------------------------------------
+
+void Math_Parser::Math_Parser_Add_Variable(string variable_key, VARIABLE_TYPE variable_value) {
+    
+    Variable_Key_List.push_back(variable_key);
+    Variable_Value_List.push_back(variable_value);
+    
+    Number_Of_Variables++;
+}
+
+// --------------------------------------------------------------------
+
+void Math_Parser::Math_Parser_Calculate() {
+    
+    for(int i = 0; i < Number_Of_Variables; ++i) {
+        
+        Symbol_Table.add_variable( Variable_Key_List[i], Variable_Value_List[i] );
+    }
+    
+    Variable_Key_List.clear();
+    Variable_Value_List.clear();
+    
+    Expression.register_symbol_table(Symbol_Table);
+    Parser.compile(Expression_String,Expression);
+    
+    Expression_Value = Expression.value();
+}
+
+// --------------------------------------------------------------------
--- main.cpp
+++ main.cpp
@@ -7,12 +7,38 @@
 //
 
 #include <iostream>
+#include "Math_Parser.hpp"
 
 using namespace std;
 
 int main( void ) {
     
-    cout << "Hello, World!\n";
+    Math_Parser Parser_fx_1;
+    Math_Parser Parser_fx_2;
+    
+    Parser_fx_1.Math_Parser_Add_Expression("x_1^2");
+    
+    Parser_fx_1.Math_Parser_Add_Variable("x_1", 2);
+    Parser_fx_1.Math_Parser_Calculate();
+    
+    cout << "Wartosc f(x)_1: " << Parser_fx_1.Math_Parser_Get_Expression_Value() << endl;
+    
+    Parser_fx_1.Math_Parser_Add_Variable("x_1", 3);
+    Parser_fx_1.Math_Parser_Calculate();
+    
+    cout << "Wartosc f(x)_1: " << Parser_fx_1.Math_Parser_Get_Expression_Value() << endl;
+    
+    Parser_fx_1.Math_Parser_Add_Expression("x_1^2");
+    Parser_fx_1.Math_Parser_Add_Variable("x_1", 2);
+    Parser_fx_1.Math_Parser_Calculate();
+    
+    Parser_fx_2.Math_Parser_Add_Expression("sqrt(x_1)");
+    Parser_fx_2.Math_Parser_Add_Variable("x_1", 2);
+    Parser_fx_2.Math_Parser_Add_Variable("x_2", 2);
+    Parser_fx_2.Math_Parser_Add_Variable("x_3", 2);
+    Parser_fx_2.Math_Parser_Calculate();
+    
+    cout << "Wartosc f(x)_2: " << Parser_fx_2.Math_Parser_Get_Expression_Value() << endl;
     
     return 0;
 }
