c5cbb2fbc850cf9efc392c4e4f06ac5ffa5e0c5e
Michał Nowak <michal.nowak061@gmail.com>
2020-03-09 01:25:09 +0100

    Rozpoczecie prac nad algorytmem NSGA

---
+++ inc/NSGA.hpp
@@ -0,0 +1,14 @@
+//
+//  NSGA.hpp
+//  TiMO_Projekt
+//
+//  Created by Michal on 08/03/2020.
+//  Copyright © 2020 Michal_Nowak & Kacper_Stenka. All rights reserved.
+//
+
+#ifndef NSGA_hpp
+#define NSGA_hpp
+
+#include <stdio.h>
+
+#endif /* NSGA_hpp */
---
+++ src/NSGA.cpp
@@ -0,0 +1,9 @@
+//
+//  NSGA.cpp
+//  TiMO_Projekt
+//
+//  Created by Michal on 08/03/2020.
+//  Copyright © 2020 Michal_Nowak & Kacper_Stenka. All rights reserved.
+//
+
+#include "NSGA.hpp"
